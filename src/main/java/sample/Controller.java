38975375cb5f938fa61e8bae0770e4f570600b33
edleijnse <ed@leijnse.info>
2020-02-18 11:08:59 +0100

    Interaction between fields added

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class Controller {
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    public void setAcdpDirectory(Event e){
+
+        System.out.println("Button clicked");
+        txtResult.setText("First Text");
+    }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,11 +1,17 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class Main extends Application {
 
@@ -14,14 +20,17 @@ public class Main extends Application {
         // Parent root = FXMLLoader.load(getClass().getResource("../../resources/main.fxml"));
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/fxml/main.fxml"));
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("src"));
 
         Parent root = loader.load();
 
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("ACDP Test Gui");
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
-    }
+        Controller myController = loader.getController();
 
+    }
 
     public static void main(String[] args) {
         launch(args);
