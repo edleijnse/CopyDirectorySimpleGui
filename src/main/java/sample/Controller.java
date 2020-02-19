d283c9829850284cc7b385b48d802bcebce8e149
edleijnse <ed@leijnse.info>
2020-02-19 16:28:23 +0100

    Preview added

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,15 +1,21 @@
 package sample;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import leijnse.info.AcdpAccessor;
 import leijnse.info.ImageRow;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -27,6 +33,9 @@ public class Controller {
     private Label lblAcdpDirectory = new Label();
 
     @FXML
+    private Label lblClickedImage = new Label();
+
+    @FXML
     public void setAcdpDirectory(Event e){
 
         System.out.println("Button setAcdpDiretory clicked");
@@ -35,6 +44,9 @@ public class Controller {
         String myChoosenDirectory = choosedDirectory.toPath().toString();
         System.out.println("choosedDirectory: " + myChoosenDirectory);
         lblAcdpDirectory.setText(myChoosenDirectory);
+        lblClickedImage.setText("");
+        Node node = null;
+        lblClickedImage.setGraphic(node);
 
 
     }
@@ -48,11 +60,44 @@ public class Controller {
 
         List<ImageRow> imageWithSomeKeywords = acdpAccessor.selectFromImageTable(true,"/media/psf/MyDrive01/acdp/acdpImage/layout", "-","-", BigInteger.valueOf(0),txtSearchKeywords.getText());
         imageWithSomeKeywords.forEach(imageRow -> {
-            listItems.getItems().add(imageRow.getDirectory()+", "+imageRow.getFile()+", " + imageRow.getIptcKeywords());
+            listItems.getItems().add(imageRow.getDirectory()+"/"+imageRow.getFile()+", keywords: " + imageRow.getIptcKeywords());
         });
         listItems.refresh();
+        lblClickedImage.setText("");
+        Node node = null;
+        lblClickedImage.setGraphic(node);
 
 
+    }
+
+    @FXML
+    public void listItemsClicked(Event e) throws FileNotFoundException {
+        System.out.println("listItems clicked");
+        lblClickedImage.setText("listItems clicked");
+        ObservableList selectedIndices = listItems.getSelectionModel().getSelectedIndices();
+
+        int myIndex = 0;
+        for(Object o : selectedIndices){
+            System.out.println("o = " + o + " (" + o.getClass() + ")");
+            myIndex = (int)o;
+            String myItemText = (String)listItems.getItems().get(myIndex);
+            System.out.println(myItemText);
+            int endItemText = myItemText.indexOf(", keywords");
+            if (endItemText>0){
+                String myImage = myItemText.substring(0,endItemText);
+                System.out.println(myImage);
+                lblClickedImage.setText(myItemText);
+                FileInputStream input = new FileInputStream(myImage);
+                Image image = new Image(input);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitHeight(300);
+                imageView.setFitWidth(300);
+                lblClickedImage.setGraphic(imageView);
+            }
+            else {
+                lblClickedImage.setText(myItemText);
+            }
+        }
     }
     private void configuringDirectoryChooser(DirectoryChooser directoryChooser) {
         // Set title for FileChooser
